fdea1aa91ab624e69a72484d80e653eafdf03ede
Paul Schrage <pas@Pauls-Laptop.local>
2026-03-31 10:32:43 -0700

    allowed for per chat emailing

--- TODO.md
+++ TODO.md
@@ -1,9 +1,11 @@
-* Be able to support multiple users who are interested in only specific groups. For example, Bob might only want messages from group A, Alice from group B, and I want messages from groups A, B, and C. 
+* ~~Be able to support multiple users who are interested in only specific groups. For example, Bob might only want messages from group A, Alice from group B, and I want messages from groups A, B, and C.~~ Done March 31, 2026
 
-* Currently saving the image as a tar file and scp'ing up to the box. TODO is build image, push to repo and pull updated image on box, restart docker all in 1 go. 
+* Currently saving the image as a tar file and scp'ing up to the box. Build image, push to repo and pull updated image on box, restart docker all in 1 go. 
 
 * Models from HuggingFace are between 7 and 16MB. I don't want to continuously run a server with that much memory just for this 1 task. Investigate cheaper options to run different models to summerize the messages. 
 
 * Add monitoring/alerting. 
 
-* Once a message has been successfully emailed, mark it as such so it doesn't get emailed twice. Possibly use a timestamp to check last successful email. 
+* Once a message has been successfully emailed, mark it as such so it doesn't get emailed twice. Possibly use a timestamp to check last successful email. Currently counting on the fact that this runs once a day and the bunyon will rotate log files. 
+
+* Add photos as attachments.
--- util/email_messages.py
+++ util/email_messages.py
@@ -4,14 +4,22 @@ emails all the messages from logs/messages.log.0 so I can add to the training da
 import os
 import logging
 import sys
-import smtplib
+import json
+from collections import defaultdict
 from datetime import datetime, timedelta
-from email.message import EmailMessage
+from zoneinfo import ZoneInfo
 from emailer import Emailer
 
-MY_EMAIL = os.getenv('MY_EMAIL')
-if not MY_EMAIL:
+# Format of EMAIL_MAPPING IS {'chat name: [list of emails], 'chat name 2': [emails], etc}
+EMAIL_MAPPING_RAW = os.getenv('EMAIL_MAPPING')
+if not EMAIL_MAPPING_RAW:
     print('no email found')
+    sys.exit(1)
+
+try:
+    EMAIL_MAPPING = json.loads(EMAIL_MAPPING_RAW)
+except json.JSONDecodeError as e:
+    print(f'EMAIL_MAPPING is not valid JSON: {e}')
     sys.exit(1)
 
 script_path = os.path.dirname(os.path.abspath(__file__))
@@ -26,14 +34,28 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-def read_messages_from_log(log_file_path):
+def read_messages_from_log(log_file_path) -> dict:
+    '''Returns dictionary like: {'chat name': [{message}, {message}, ...]}
+    '''
+    messages = defaultdict(list)
     try:
         with open(log_file_path) as f:
-            messages = f.read()
+            for line in f:
+                message = json.loads(line)
+                message_time = datetime.fromtimestamp(message['ts'], tz=ZoneInfo('America/Los_Angeles'))
+                messages[message['chat']].append({'chat': message['chat'], 
+                                                  'sender': message['sender'], 
+                                                  'body': message['body'], 
+                                                  'time': message_time.strftime('%H:%M:%S %Z')})
 
     except Exception as e:
         logger.error(f"Error reading log file {log_file_path}: {e}")
         messages = None
+
+    for v in messages.values():
+        for m in v:
+            if not m['body']:
+                m['body'] = "(empty message, probably sent a photo)"
 
     return messages
 
@@ -57,17 +79,20 @@ if __name__ == "__main__":
         formatted_date = yesterday.strftime("%d %B %Y")
         
         # Send email with message content
-        success = emailer.send(
-            sender=sender_email,
-            recipients=[MY_EMAIL],
-            subject=f'WhatsApp Messages for {formatted_date}',
-            body=messages
-        )
-        
-        if success:
-            logger.info(f"Successfully sent messages to {MY_EMAIL}")
-        else:
-            logger.error("Failed to send email")
-            sys.exit(1)
+        for k, v in EMAIL_MAPPING.items():
+            if len(messages[k]) == 0:
+                continue
+            body = '\n'.join(f"{m['time']} - {m['sender']}: {m['body']}" for m in messages[k])
+            success = emailer.send(
+                sender=sender_email,
+                recipients=v,
+                subject=f'{k} WhatsApp Messages for {formatted_date}',
+                body=body
+            )
+            
+            if success:
+                logger.info(f"Successfully sent messages to {v}")
+            else:
+                logger.error(f"Failed to send email for {k}")
     else:
         logger.info("No messages found to send")
